d03ad0fc1934a8d5558119165c8a90f9ccc11664
math2018avan <math2018avan@gmail.com>
2019-03-17 00:00:38 +0800

    model for login user

---
+++ Supermarket/Resources/LoginResource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supermarket.Resources
+{
+    public class LoginResource
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
