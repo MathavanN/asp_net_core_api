5662031de4ea118f1a28a13365c903e5b06a1688
math2018avan <math2018avan@gmail.com>
2019-03-16 23:56:39 +0800

    Asp Net Identity Solution
    Identity Solution added
    Db initialization
    ApplicationUser class created to customize AspNetUser

---
+++ Supermarket.Identity/Context/AuthenticationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Supermarket.Identity.Models;
+
+namespace Supermarket.Identity.Context
+{
+    public class AuthenticationContext : IdentityDbContext
+    {
+        public AuthenticationContext(DbContextOptions<AuthenticationContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
