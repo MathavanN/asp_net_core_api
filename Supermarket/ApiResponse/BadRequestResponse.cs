a8ec0a54d48929a77e84b8b7d66207c4e7148340
math2018avan <math2018avan@gmail.com>
2019-03-30 00:27:47 +0800

    API error response
    400, 404 standard error response format added (similar to ASP..CORE model validation result)

---
+++ Supermarket/ApiResponse/BadRequestResponse.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Supermarket.ApiResponse
+{
+    public class BadRequestResponse : ValidationProblemDetails
+    {
+        public Guid TraceId { get; set; }
+        public BadRequestResponse(string error) : base(new Dictionary<string, string[]> { { "error", new[] { error } } })
+        {
+            TraceId = Guid.NewGuid();
+            Title = HttpStatusCode.BadRequest.ToString();
+        }
+    }
+}
