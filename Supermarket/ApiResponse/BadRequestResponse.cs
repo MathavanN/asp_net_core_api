5f82b834cdd2ebac543e313de380103aeb564b77
math2018avan <math2018avan@gmail.com>
2019-03-30 14:27:04 +0800

    Status code added
    Status code added

--- Supermarket/ApiResponse/BadRequestResponse.cs
+++ Supermarket/ApiResponse/BadRequestResponse.cs
@@ -8,10 +8,12 @@ namespace Supermarket.ApiResponse
     public class BadRequestResponse : ValidationProblemDetails
     {
         public Guid TraceId { get; set; }
+
         public BadRequestResponse(string error) : base(new Dictionary<string, string[]> { { "error", new[] { error } } })
         {
             TraceId = Guid.NewGuid();
             Title = HttpStatusCode.BadRequest.ToString();
+            Status = (int)HttpStatusCode.BadRequest;
         }
     }
 }
--- Supermarket/ApiResponse/NotFoundResponse.cs
+++ Supermarket/ApiResponse/NotFoundResponse.cs
@@ -8,10 +8,24 @@ namespace Supermarket.ApiResponse
     public class NotFoundResponse : ValidationProblemDetails
     {
         public Guid TraceId { get; set; }
+
         public NotFoundResponse(string error) : base(new Dictionary<string, string[]> { { "error", new[] { error } } })
         {
             TraceId = Guid.NewGuid();
             Title = HttpStatusCode.NotFound.ToString();
+            Status = (int)HttpStatusCode.NotFound;
         }
+
+        ////In feature, to log the not found response
+        //public NotFoundResponse(string error, ILogger logger) : base(new Dictionary<string, string[]> { { "error", new[] { error } } })
+        //{
+        //    TraceId = Guid.NewGuid();
+        //    Title = HttpStatusCode.NotFound.ToString();
+        //    Status = (int)HttpStatusCode.NotFound;
+        //    logger.LogInformation(JsonConvert.SerializeObject(this,
+        //                                                       Formatting.Indented,
+        //                                                       new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() }
+        //                                                      ));
+        //}
     }
 }
