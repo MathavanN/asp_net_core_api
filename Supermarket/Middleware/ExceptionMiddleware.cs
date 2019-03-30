77e38c2818585881d9828d94a48a6c3a6a7a7151
math2018avan <math2018avan@gmail.com>
2019-03-30 14:30:13 +0800

    Custom error handle
    Global Error Handling added

---
+++ Supermarket/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Supermarket.ApiResponse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Supermarket.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"API Error: {ex}");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError; // 500 if unexpected
+
+            ////Refer https://stackoverflow.com/questions/38630076/asp-net-core-web-api-exception-handling
+
+            var result = JsonConvert.SerializeObject(
+                                                        new InternalServerErrorResponse(exception.Message),
+                                                        Formatting.Indented,
+                                                        new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() }
+                                                    );
+            _logger.LogError(result);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
