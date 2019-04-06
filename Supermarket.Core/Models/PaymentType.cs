3c48cfebc2b820eaddb75f073402d81960095319
math2018avan <math2018avan@gmail.com>
2019-04-06 11:52:20 +0800

    Create PaymentType.cs
    payment type model added

---
+++ Supermarket.Core/Models/PaymentType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Supermarket.Core.Models
+{
+    class PaymentType : IEntity
+    {
+        public int Id { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string PayementType { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public DateTime DateModified { get; set; }
+    }
+}
