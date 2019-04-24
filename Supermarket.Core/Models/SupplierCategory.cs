f150e6173a4d2b580b0d6e9e05c0a17c599e1024
math2018avan <math2018avan@gmail.com>
2019-04-24 22:54:25 +0800

    Create SupplierCategory.cs
    SupplierCategory model added

---
+++ Supermarket.Core/Models/SupplierCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Supermarket.Core.Models
+{
+    public class SupplierCategory : IEntity
+    {
+        public int Id { get; set; }
+
+        public int  SupplierId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public DateTime DateModified { get; set; }
+    }
+}
