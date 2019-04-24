91c0e19430dd5d1b2b75241b1f0632b005c5aa0e
math2018avan <math2018avan@gmail.com>
2019-04-24 22:53:06 +0800

    Update Category.cs
    Data Annotation removed

--- Supermarket.Core/Models/Category.cs
+++ Supermarket.Core/Models/Category.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Supermarket.Core.Models
 {
     public class Category : IEntity
     {
         public int Id { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
         public string Name { get; set; }
         public IList<Product> Products { get; set; } = new List<Product>();
         public DateTime DateAdded { get; set; }
