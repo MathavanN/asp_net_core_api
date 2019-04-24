c9573381e152dd132cc897368c5da097801b4c19
math2018avan <math2018avan@gmail.com>
2019-04-24 23:02:03 +0800

    Create Coverage.bat
    CodeCov report generation bat file added

---
+++ Supermarket.UnitTests/Coverage.bat
@@ -0,0 +1,6 @@
+﻿@ECHO OFF
+dotnet test --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory ./CoverageReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=CoverageReports\Coverage\ /p:CoverletOutputFormat=cobertura /p:Exclude="[xunit.*]*
+
+dotnet reportgenerator "-reports:CoverageReports\Coverage\coverage.cobertura.xml" "-targetdir:CoverageReports\Coverage" -reporttypes:HTML;HTMLSummary
+
+start CoverageReports\Coverage\index.htm
