884d2ec1f3a9e3436424c6d84e342fdc21de9e58
math2018avan <math2018avan@gmail.com>
2019-04-24 22:58:06 +0800

    Create CountriesController.cs
    Country api methods added

---
+++ Supermarket/V1/Controllers/CountriesController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Supermarket.ApiResponse;
+using Supermarket.Core.Models;
+using Supermarket.Core.Repositories.Contracts;
+using Supermarket.V1.Dtos.CountryDtos;
+
+namespace Supermarket.V1.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private IRepositoryWrapper _repositoryWrapper;
+        private readonly IMapper _mapper;
+
+        public CountriesController(IRepositoryWrapper repositoryWrapper, IMapper mapper)
+        {
+            _repositoryWrapper = repositoryWrapper;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var countries = await _repositoryWrapper.Country.ListAllCountriesAsync();
+
+            var countriesDtos = _mapper.Map<IEnumerable<Country>, IEnumerable<CountryDto>>(countries);
+
+            return Ok(countriesDtos);
+        }
+
+        [HttpGet("{id}", Name = "CountryById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(NotFoundResponse), StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> GetCountryById(int id)
+        {
+            var dbCountry = await _repositoryWrapper.Country.FindByIdAsync(id);
+            if (dbCountry == null)
+                return NotFound(new NotFoundResponse("Country not found"));
+
+            var countryDto = _mapper.Map<Country, CountryDto>(dbCountry);
+
+            return Ok(countryDto);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> PostAsync([FromBody] CreateCountryDto createCountryDto)
+        {
+            var country = _mapper.Map<CreateCountryDto, Country>(createCountryDto);
+
+            await _repositoryWrapper.Country.AddCountryAsync(country);
+
+            var countryDto = _mapper.Map<Country, CountryDto>(country);
+
+            return CreatedAtRoute("CountryById", new { id = countryDto.Id }, countryDto);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(NotFoundResponse), StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> PutAsync(int id, [FromBody] SaveCountryDto saveCountryDto)
+        {
+            var dbCountry = await _repositoryWrapper.Country.FindByIdAsync(id);
+            if (dbCountry == null)
+                return NotFound(new NotFoundResponse("Country not found"));
+
+            dbCountry.Name = saveCountryDto.Name;
+            dbCountry.DateModified = saveCountryDto.DateModified;
+
+            await _repositoryWrapper.Country.UpdateCountryAsync(dbCountry);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(NotFoundResponse), StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var dbCountry = await _repositoryWrapper.Country.FindByIdAsync(id);
+            if (dbCountry == null)
+                return NotFound(new NotFoundResponse("Country not found"));
+
+            await _repositoryWrapper.Country.DeleteCountryAsync(dbCountry);
+
+            return NoContent();
+        }
+    }
+}
