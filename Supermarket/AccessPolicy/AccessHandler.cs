b4d4854e115ab084e47773ec38abe67be89099b7
math2018avan <math2018avan@gmail.com>
2019-06-30 01:11:27 +0800

    access handler added

---
+++ Supermarket/AccessPolicy/AccessHandler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+
+namespace Supermarket.AccessPolicy
+{
+    public class AccessHandler : AuthorizationHandler<AccessRequirement>, IAuthorizationRequirement
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, AccessRequirement requirement)
+        {
+            if (context.User.IsInRole(requirement.Role))
+            {
+                // Call 'Succeed' to mark current requirement as passed
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
