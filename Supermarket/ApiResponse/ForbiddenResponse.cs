4efc33e0f8e5311e2eb19bc62ee42fc44044e7df
math2018avan <math2018avan@gmail.com>
2019-06-30 01:11:38 +0800

    403 error response added

---
+++ Supermarket/ApiResponse/ForbiddenResponse.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Supermarket.ApiResponse
+{
+    public class ForbiddenResponse : ValidationProblemDetails
+    {
+        public Guid TraceId { get; set; }
+
+        public ForbiddenResponse(string error) : base(new Dictionary<string, string[]> { { "error", new[] { error } } })
+        {
+            TraceId = Guid.NewGuid();
+            Title = HttpStatusCode.Forbidden.ToString();
+            Status = (int)HttpStatusCode.Forbidden;
+        }
+    }
+}
